c30c425725e00ada2571cc543e7bc5aee30e067f
nifferblue <nyabokejeniffer2@gmal.com>
2024-11-21 20:49:22 +0300

    third committ

--- Node2/index.js
+++ Node2/index.js
@@ -1,77 +1,103 @@
-const express = require('express')
-
-const app =express();
-
+const express = require('express');
 const path = require('path');
 
-app.use(express.static(path.join(__dirname,'public')));
+const app = express();
 
+const {MongoClient} = require('mongodb')
 
-app.get('/project',(req,res)=>{
+const url = "mongodb://localhost:27017"
 
-  res.sendFile(path.join(__dirname,'views','Home.html'));
+async function main() {
+  const client = new MongoClient(url);
 
-});
-app.get('/arrays',(req,res)=>{
+  await client.connect();
+  console.log('Connected successfully to the server')
+}
 
-  res.sendFile(path.join(__dirname,'views','arrays.html'));
+const databaseList = await client.db().admin().listDatabases();
 
-});
-app.get('/project/events',(req,res)=>{
+console.log('Databases');
 
-  res.sendFile(path.join(__dirname,'views','events.html'));
-
-});
-app.get('/project/form',(req,res)=>{
-
-  res.sendFile(path.join(__dirname,'views','form.html'));
-
-});
-app.get('/project/index',(req,res)=>{
-
-  res.sendFile(path.join(__dirname,'views','index.html'));
-
-});
-app.get('/project/jokeGenerator',(req,res)=>{
-
-  res.sendFile(path.join(__dirname,'views','jokeGenerator.html'));
-
-});
-app.get('/project/Main',(req,res)=>{
-
-  res.sendFile(path.join(__dirname,'views','Main.html'));
-
+databaseList.databases.array.forEach(db => {
+  console.log(db.name)
+  
 });
 
+await client.close()
 
 
-// build login page and signup and a request handler that serves you when you request them in views username,password for signup
 
-// app.get('/',(req,res)=>{
-//   res.send('Welcome to my Web Site ');
-// });
 
-// app.get('/arrays',(req,res)=>{
-//   res.send('ARRAYS  ');
-// });
 
-// app.get('/events',(req,res)=>{
-//   res.send('Events');
-// });
+// Middleware to parse form data
+app.use(express.urlencoded({ extended: true }));
 
-// app.get('/form',(req,res)=>{
-//   res.send('FORM');
-// });
+// Serve static files from 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.get('/jokes',(req,res)=>{
-//   res.send('JOKES');
-// });
+// Serve the project pages
+app.get('/project', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'Home.html'));
+});
 
-app.listen(3000,()=>{
+app.get('/arrays', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'arrays.html'));
+});
+
+app.get('/project/events', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'events.html'));
+});
+
+app.get('/project/form', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'form.html'));
+});
+
+app.get('/project/index', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/project/jokeGenerator', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'jokeGenerator.html'));
+});
+
+app.get('/project/Main', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'Main.html'));
+});
+
+// Serve login and signup pages
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'loginSignup.html')); // Assume the file is loginSignup.html
+});
+
+app.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'loginSignup.html')); // Same page, just a different link
+});
+
+// Handle login request
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  console.log('Login attempt:', { username, password });
+
+  // For demonstration purposes, you could add validation here (e.g., check against a database)
+  if (username === 'user' && password === 'password') {
+    res.send('Login successful');
+  } else {
+    res.send('Invalid credentials');
+  }
+});
+
+// Handle signup request
+app.post('/signup', (req, res) => {
+  const { username, password } = req.body;
+  console.log('Sign Up attempt:', { username, password });
+
+  // Here, you'd typically save the user info to a database
+  res.send(`Signup successful for user: ${username}`);
+});
+
+// Start the server
+app.listen(3000, () => {
   console.log('Server is running on port 3000');
-})
-
-/* install nodemon 'npm i -g nodemon' to avoid refreshing each time run as nodemon index.js */
-
+});
 
 
